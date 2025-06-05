391f49a4bf70bce97ec19efb378e058fcb8a7248
umer <umery1730@gmail.com>
2025-06-06 00:21:18 +0500

    ADD

---
+++ week9/task2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int numbers[5];
+    numbers[0]=15;      
+    numbers[1]=12;
+    numbers[2]=13;
+    numbers[3]=19;
+    numbers[4]=10;
+    
+    cout<<"The second element at location number[0] is:"<<numbers[1]<<endl;
+
+    cout<<"The fourth element at location number[0] is:"<<numbers[3]<<endl;
+
+}
---
+++ week9/task3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int numbers[5];
+    cout<<"Enter first number:";
+    cin>>numbers[0];
+    cout<<"Enter second number:";      
+    cin>>numbers[1];
+    cout<<"Enter third number:";
+    cin>>numbers[2];
+    cout<<"Enter fourth number:";
+    cin>>numbers[3];
+    cout<<"Enter fifth number:";
+    cin>>numbers[4];
+    cout<<"The first element at location number[0] is:"<<numbers[0]<<endl;
+    cout<<"The fifth element at location number[0] is:"<<numbers[4]<<endl;
+}
