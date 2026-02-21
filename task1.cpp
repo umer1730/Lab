1df6b225c01005514fe8f30d5495931317879e0a
Umer Yaseen <umery1730@gmail.com>
2026-02-21 13:48:33 +0500

    add

--- task1.cpp
+++ task1.cpp
@@ -1,13 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-main()
-{
-    int num1,num2,sum;
+int main()
+{   cout<<"file"
+    int num1;
+    int num2;
+    int num3;
+    int sum;
+    
     cout<<"Enter the first number:";
     cin>>num1;
     cout<<"Enter the second number:";
     cin>>num2;
     sum=num1 + num2;
     cout<<"Sum:"<<sum;
+    return 0;
 
 }
