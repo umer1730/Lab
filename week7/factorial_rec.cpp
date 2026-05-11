601a8eda129288bfe15206e67cd6ecf36a317855
UMER YASEEN <umery1730@gmail.com>
2026-05-11 17:45:59 +0500

    factorial reciprocal

---
+++ week7/factorial_rec.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int fact(int val){
+    if (val == 0 || val == 1){
+        return 1;
+    } 
+    else{
+        return val * fact(val-1);
+    }
+}
+int main(){
+    cout<<fact(4);
+    return 0;
+}
